3d583d5d25f7520e564773ab0ae57fcba372adb8
Opilane TTHK <opilane@tthk.ee>
2021-11-16 16:25:03 +0200

    week12

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace veggies_and_fruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruitpath = @"C:\Users\opilane\samples\fruit.txt";
+            string[] veggieData = File.ReadAllLines(fruitpath);
+            string veggiepath = @"C:\Users\opilane\samples\veggie.txt";
+            string[] fruitData = File.ReadAllLines(veggiepath);
+
+            File.WriteAllLines(fruitpath, fruitData);
+            File.WriteAllLines(veggiepath, veggieData);
+        }
+    }
+}
